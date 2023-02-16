6ce935ae87cf5d751d6fce6749e1e6313ea03dc0
Angel Rodes <53089531+angelrodes@users.noreply.github.com>
2023-02-16 18:05:43 +0100

    Update START.m
    Extra plots added (commented). 
    Uncomment for extra graphical outputs used in Roberts et al. (in prep, 2023)

--- START.m
+++ START.m
@@ -4,11 +4,6 @@
 % angelrodes@gmail.com
 % 2021
 
-%% TODO list
-% Solve textscan in input_sample_data in octave (only first sample taken)
-%    - explore dlmread instead of textscan
-%    - maybe ignore sample names (we don't really use them beyond listing)
-%        dlmread(filename,',',1,1)
 
 %% Ask what to do
 btn = questdlg ('Select', 'NUNAIT calculator', 'Run simulation', 'Display results', 'Run simulation');
@@ -48,12 +43,9 @@ end
 display_results(selectedfile);
 plot_results(selectedfile);
 
-
-%% todo
-% allow several files at a time by taking file salection out of input
-% samples
-% display credits and VERSION CONTROL (consts (general),climate data, fitting script, muon contribution, nuna_model)
-% display result text
+% Uncomment for extra plots
+% plot_individual_profiles(selectedfile);
+% plot_landscape_evolution(selectedfile);
 
 end
 
